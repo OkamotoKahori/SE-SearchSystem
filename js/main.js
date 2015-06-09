44a464b91beea1f434052796d7d6520de860d2df
OkamotoKahori <kahori.n811@gmail.com>
2015-06-09 17:51:22 +0900

    cssを追加

--- js/main.js
+++ js/main.js
@@ -12,19 +12,18 @@ $(function() {
                     delimiter: "\n",
                     header: false,
                     dynamicTyping: true,
-                    preview: 100,
+                    preview: 101,
                     step: function(data, file, inputElem) {
                         csvData.push(data.results[0]);
                     }
                 });
  
-                // [縦][横]、[0][0]:タイトル、[0][1]:グループ、[0][2]:オノマトペ、[99]まで
+                // [縦][横]、[0][0]:タイトル、[0][1]:グループ、[0][2]:オノマトペ、[100]まで
                 // 検索部分
                 for (var i = 1; i < csvData.length - 1; i++) {
                     console.log(csvData[i][0]);
                     if (csvData[i][2] === query) {
                         $('#result').append('<p><a href="./sound/' + csvData[i][0] + '.wav" target="_blank">' + csvData[i][0] + '</a></p>');
-
                     }
                 }
 
